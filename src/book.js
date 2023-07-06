30a41029c2fd6113ccae319ed50d82cba4130fd3
Mary Bruff <marybruff5@gmail.com>
2023-07-06 13:23:42 -0600

    pass first three tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,32 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
 }
+
+function buildMainCharacter(name,age, pronouns){
+  var character = {
+  name: name,
+  age: age,
+  pronouns: 'she/her'
+  }
+  return character
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review);
+  }
+}
+
+function calculatePageCount(title, pageCount) {
+  title 
+}
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
   // writeBook,
   // editBook
 }
