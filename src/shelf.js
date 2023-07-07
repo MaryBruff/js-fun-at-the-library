702844f154a4748374d1d407260f9f977d7b8caa
Mary Bruff <marybruff5@gmail.com>
2023-07-07 15:24:40 -0600

    pass unshelfBook  function tests

--- src/shelf.js
+++ src/shelf.js
@@ -4,11 +4,18 @@ if (shelf.length < 3){
 }
 }
 
-// add books to scifi shelf
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      return shelf.splice(i, 1);
+    }
+  }
+}
+
 
 module.exports = {
   shelfBook,
-  // unshelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
