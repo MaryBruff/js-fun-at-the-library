c6732a08265417686802b3928ba29297d7df514f
Mary Bruff <marybruff5@gmail.com>
2023-07-08 13:22:42 -0600

    pass all checkoutBook tests

--- src/library.js
+++ src/library.js
@@ -6,12 +6,26 @@ function createLibrary(name, shelves) {
   return libraryName
 }
 
+function addBook(libraryName,book) {
+  libraryName.shelves[book.genre].push(book)
+}
+
+function checkoutBook(libraryName, title, genre) {
+  for (var i = 0; i < libraryName.shelves[genre].length; i++) {
+    var book = libraryName.shelves[genre][i];
+    if (book.title === title) {
+      libraryName.shelves[genre].splice(i, 1);
+      return `You have now checked out ${title} from the ${libraryName.name}.`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${title} available at the ${libraryName.name}.`;
+}
 
 
 
 module.exports = {
   createLibrary,
-  // addBook,
-  // checkoutBook,
-  // takeStock
+  addBook,
+  checkoutBook,
+  takeStock
 };
